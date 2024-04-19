7e9931378e79496dca28e9f07f9843009ce2b40b
Michael Schellenberger Costa <miscco@nvidia.com>
2024-04-19 10:55:44 +0200

    Try to work around reference issues with thrust tuple

--- thrust/thrust/tuple.h
+++ thrust/thrust/tuple.h
@@ -160,7 +160,9 @@ struct tuple : public _CUDA_VSTD::tuple<Ts...>
   using super_t = _CUDA_VSTD::tuple<Ts...>;
   using super_t::super_t;
 
-  tuple() = default;
+  _CCCL_HOST_DEVICE tuple()
+      : super_t()
+  {}
 
   template <class _TupleOfIteratorReferences,
             _CUDA_VSTD::__enable_if_t<__is_tuple_of_iterator_references<_TupleOfIteratorReferences>::value, int> = 0,
